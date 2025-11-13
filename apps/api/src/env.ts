d22d5a0ddbebe5f30ab418409c28c2ecde874878
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-11-13 10:44:30 -0400

    feat: integrate Redis caching into user retrieval process and update environment configuration

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -1,8 +1,10 @@
+import { schema as cacheEnv } from '@org/cache/env'
 import { schema as databaseEnv } from '@org/database/env'
 import { z } from '@org/validation/zod'
 
 const schema = z.object({
   ...databaseEnv.shape,
+  ...cacheEnv.shape,
   FRONT_END_URL: z.url(),
 })
 
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,30 @@
 import './env'
+import { cacheClient } from '@org/cache'
+import { database } from '@org/database'
+
 import { Elysia } from 'elysia'
 
 const app = new Elysia()
-  .get('/', () => ({
-    message: 'Hello Zomboid API with Elysia 🦊🦊🦊',
-  }))
+  .get('/', async () => {
+    try {
+      const cachedUsers = await cacheClient.get('users')
+
+      if (cachedUsers) {
+        return JSON.parse(cachedUsers)
+      }
+
+      const users = await database.query.usersTable.findMany()
+
+      if (!cachedUsers) {
+        await cacheClient.set('users', JSON.stringify(users), 'EX', 60)
+      }
+
+      return users
+    } catch (error) {
+      console.error(error)
+      return { error: 'Failed to fetch users' }
+    }
+  })
   .get('/health', () => ({
     message: 'OK',
   }))
