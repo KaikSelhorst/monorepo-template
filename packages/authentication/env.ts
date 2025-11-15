d9e85830eccb4d96dfb1169da8c34ebcf497d211
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-11-14 22:05:57 -0400

    feat: extend authentication environment schema to include database schema properties

--- packages/authentication/env.ts
+++ packages/authentication/env.ts
@@ -1,6 +1,8 @@
+import { schema as databaseSchema } from '@org/database/env'
 import { z } from '@org/validation/zod'
 
 export const schema = z.object({
+  ...databaseSchema.shape,
   DISCORD_CLIENT_ID: z.string().min(1),
   DISCORD_CLIENT_SECRET: z.string().min(1),
   ORIGIN_ALLOWED: z
