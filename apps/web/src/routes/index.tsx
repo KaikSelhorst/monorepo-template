883dca0cfd3f8c12e5992889660c17ad9779cae4
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-11-15 09:08:27 -0400

    feat: implement user authentication UI with sign-in and sign-out buttons in the main app component

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,20 +1,41 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { auth } from '@/lib/auth'
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
 function App() {
+  const { data } = auth.useSession()
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <hgroup>
-        <h1 className="text-2xl">
-          Building your own Zomboid builds with ease.
-        </h1>
-        <a href="https://store.steampowered.com/app/108600/Project_Zomboid/">
-          Steam
-        </a>
-      </hgroup>
+    <div className="flex p-4 gap-4">
+      {data ? (
+        <button
+          type="button"
+          className="p-4 bg-lime-400 text-black rounded-sm h-fit"
+          onClick={() => auth.signOut()}
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="p-4 bg-lime-400 text-black rounded-sm h-fit"
+          onClick={() =>
+            auth.signIn.social({
+              provider: 'discord',
+              callbackURL: location.origin,
+            })
+          }
+        >
+          Login With Discord
+        </button>
+      )}
+
+      <pre className="bg-zinc-900 p-4 rounded-sm">
+        {JSON.stringify(data, null, 2)}
+      </pre>
     </div>
   )
 }
