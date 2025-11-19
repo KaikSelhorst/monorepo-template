be7b1a213b979bdf3b9a0225e2527b14a4f63194
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-11-19 10:22:41 -0400

    feat: update index page to use ArrowRight icon and link template button for improved navigation

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@org/design-system/components/ui/button'
-import { Star } from '@org/design-system/components/ui/icons'
+import { ArrowRight } from '@org/design-system/components/ui/icons'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -10,14 +10,20 @@ function RouteComponent() {
   return (
     <section className="container">
       <div className="w-full h-[50vh] bg-primary rounded-md flex justify-center items-center">
-        <h1 className="text-primary-foreground text-6xl font-medium">
+        <h1 className="text-primary-foreground text-4xl font-medium text-center md:text-6xl">
           Elysia + Tanstack Router
         </h1>
       </div>
       <nav className="mt-4 flex justify-center gap-4">
-        <Button>Use Template</Button>
-        <Button mode="icon">
-          <Star />
+        <Button asChild>
+          <a
+            href="https://github.com/KaikSelhorst/monorepo-template"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Use Template
+          </a>
+          <ArrowRight />
         </Button>
       </nav>
     </section>
